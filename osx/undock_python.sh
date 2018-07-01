4994ea459d6a1db10b48e58ed71c48a251033931
Luca Viola <luca.viola@gmail.com>
2018-07-01 09:51:34 +0200

    Added proper path resolution through symbolic link to detetect the proper Info.plist file for the current python3 app

--- osx/undock_python.sh
+++ osx/undock_python.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-version=`python3 --version | cut -d " " -f 2`
-path="/usr/local/Cellar/python/$version/Python*3.app/Contents"
-info_file="$path/Info.plist"
+python3_path1=`which python3`
+python3_link_path1=`readlink $python3_path1`
+
+dir1=`dirname $python3_path1`
+dir2=`echo -n $python3_link_path1 | cut -d "/" -f 2-`
+
+python3_path2="`dirname $dir1`/$dir2"
+python3_link_path2=`readlink $python3_path2`
+
+rdir1=`dirname $python3_path2`
+rdir2=`echo -n $python3_link_path2 | cut -d "/" -f 2-`
+
+python3_real_path="`dirname $rdir1`/$rdir2"
+
+pdir1=`dirname $python3_real_path`
+pdir2=`dirname $pdir1`
+
+info_dir="$pdir2/Resources/Python.app/Contents"
+info_file="$info_dir/Info.plist"
 
 lines=`wc -l $info_file | awk '{print $1}'`
 lines=$((lines-2))
@@ -15,5 +31,5 @@ if [ -z $(grep "LSUIElement" $info_file) ]; then
   echo -e "\t<true/>" >> $info_file
   tail -n 2 ./Info.plist.bck >> $info_file
 else
-  echo "FOUND"
+  echo "File seems already patched - skipping"
 fi
