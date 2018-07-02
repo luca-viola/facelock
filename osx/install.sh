6a93ed9587a6cbbbf98e0ed80451c75e0a30a17f
lviola <luca.viola@rumbo.com>
2018-07-02 10:08:36 +0200

    made s install script work with sudo for the python requirements, did some polishing on .gitignore

--- osx/install.sh
+++ osx/install.sh
@@ -2,6 +2,4 @@
 
 brew install python3
 brew install opencv
-/usr/local/bin/python3 -m pip install -r ../requirements_osx.txt
-
-
+sudo /usr/local/bin/python3 -m pip install -r ../requirements_osx.txt
