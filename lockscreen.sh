d17acce761a8d3916554e403111ff934d21f089e
lviola <luca.viola@lastminute.com>
2018-07-06 23:32:00 +0200

    universal-ish script to lock screen in osx and linux

--- lockscreen.sh
+++ lockscreen.sh
@@ -5,5 +5,18 @@ OS=`uname -s | cut -d "_" -f 1`
 if [ "$OS" == "Darwin" ]; then
   /bin/echo 'tell application "Finder" to sleep' | /usr/bin/osascript
 else
-  /usr/bin/cinnamon-screensaver-command  --lock
+  case "$XDG_CURRENT_DESKTOP" in
+   *CINNAMON*|*Cinnamon*|*cinnamon*)
+     echo /usr/bin/cinnamon-screensaver-command  --lock
+     ;;
+   *MATE*|*Mate*|*mate*)
+     echo /usr/bin/mate-screensaver-command --lock
+     ;;
+   *GNOME*|*Gnome*|*gnome*)
+     echo /usr/bin/gnome-screensaver-command --lock
+     ;;
+     *)
+       break
+       ;;
+  esac
 fi
