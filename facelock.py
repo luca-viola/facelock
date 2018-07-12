efccda6057410381a7e6916bba458fed626538e5
lviola <luca.viola@rumbo.com>
2018-07-12 16:21:09 +0200

    Added command line parameters, put an option to launch as a daemon

--- facelock.py
+++ facelock.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+import getopt
 
 from qtImports import *
 
@@ -11,7 +12,18 @@ from SystemTrayGUIBuilder import SystemTrayGUIBuilder
 from CameraProbe import CameraProbe
 from FaceRecognitionBuilder import FaceRecognitionBuilder
 from ConfigurationChecker import ConfigurationChecker
+from __init__ import __version__
+
 from nohup import nohup
+
+detach = False
+
+def usage():
+  print('facelock.py [-v] [-h] [-d]')
+  print('  -v | --version : print version')
+  print('  -h | --help    : print this message')
+  print('  -d | --detach  : go in background and detach from the tty')
+
 
 def main():
   os.chdir(os.path.dirname(sys.argv[0]))
@@ -45,5 +57,27 @@ def main():
   trayIconGUI.show()
   app.exec_()
 
+
+argv = sys.argv[1:]
+
+try:
+  opts, args = getopt.getopt(argv, "hvd", ["detach", "help", "version"])
+except getopt.GetoptError:
+  usage()
+  sys.exit(2)
+for opt, arg in opts:
+  if opt in ('-h', '--help'):
+    usage()
+    sys.exit()
+  elif opt in ("-v", "--version"):
+    print("Version: "+__version__)
+    sys.exit()
+  elif opt in ("-d", "--detach"):
+    detach = True
+
+
 if __name__ == '__main__':
-    nohup(main)
+    if detach:
+      nohup(main)
+    else:
+      main()
